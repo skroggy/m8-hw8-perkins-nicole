d6c18c95b65acc466093e658b217a0751a5106b0
Nicole Perkins <perkinsn@ufl.edu>
2021-03-08 20:14:02 -0600

    Add hover effects to main menu labels

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-// It is always helpful to use comments in your code!
+var mainLabels = document.getElementsByClassName('main-menu-labels');
+var subLabels = document.getElementsByClassName('sub-menu');
+
+mainLabels[0].addEventListener('mouseenter', 
+    function() {
+    subLabels[0].classList.remove('hide');
+});
+
+mainLabels[0].addEventListener('mouseleave', 
+    function() {
+    subLabels[0].classList.add('hide');
+});
+
+mainLabels[1].addEventListener('mouseenter', 
+    function() {
+    subLabels[1].classList.remove('hide');
+});
+
+mainLabels[1].addEventListener('mouseleave', 
+    function() {
+    subLabels[1].classList.add('hide');
+});
+
+mainLabels[2].addEventListener('mouseenter', 
+    function() {
+    subLabels[2].classList.remove('hide');
+});
+
+mainLabels[2].addEventListener('mouseleave', 
+    function() {
+    subLabels[2].classList.add('hide');
+});
